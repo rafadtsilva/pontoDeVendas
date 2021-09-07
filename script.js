b0251689c4642e3db3eb4334d937aec46cd6775f
Rafael Trindade <rafaeltrindade@outlook.com>
2021-09-07 17:26:42 -0300

    feat: iniciado mais algumas modificações no javascript

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 var description
 var amount
 var price
+var ok
+
 $("#ok").click(lerDados)
 
 function lerDados() {
+
   description = $("#description").val();
   amount = parseInt($("#amount").val());
   price = parseInt($("#price").val());
+  
+  if(isNaN(amout) || isNaN(price) || description == ""){
+    console.log("Preencha todos os campos corretamente")
+  } else {
+    
+    $("#showDescription").text(`${description}`)
 
-  console.log(description)
-  console.log(amount*price)
-  // console.log(price)
+    console.log(description)
+    console.log(amount*price)
+  
+    $("#description").val(" ");
+    $("#amount").val(" ");
+    $("#price").val(" ")
+    
+  }
+
+
 }
