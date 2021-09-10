cbd53a6d0efa9db9966f200bf31614af01a8fb08
Rafael Trindade <rafaeltrindade@outlook.com>
2021-09-10 16:32:17 -0300

    feat: começando a implementar alguns verificadores nos inputs

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ var ok
 var twoDecimal
 var realFormat;
 var listNumber = 0;
-var nota;
+var totalValue = $("#valor-total");
+var totalValueAcc = 0;
+const testDescription = /\D{2,}/;
+const testPrice = /[0-9]{3},[0-9]{2}/;
+const testAmount = /[0-2][0-9]/;
 
 $("#ok").click(lerDados)
 
@@ -14,24 +18,35 @@ function lerDados() {
   description = $("#description").val();
   amount = parseInt($("#amount").val());
   price = parseFloat($("#price").val());
+  console.log(testDescription.test(description))
+
   
-  if(isNaN(amount) || isNaN(price) || description == ""){
+  
+  if(testDescription.test(description) !== true || testAmount.test(amount) || description == ""){
     console.log("Preencha todos os campos corretamente")
   } else {
     
     $("#showDescription").text(`${description} (x${amount})`)
 
-    // twoDecimal = (amount*price).toFixed(2);
+    //adiciona a descrição do produto inserido a nota
     $("#nota-body").append(`<tr>
-                              <td>${listNumber}</td>
-                              <td class="text-start">${description}</td>
-                              <td>R$ ${price}</td>
+                              <td class="pe-3">${listNumber+1}</td>
+                              <td id="description" class="text-start"><span>${description}</span></td>
+                              <td class="px-2">R$ ${price}</td>
                               <td>${amount}</td>
                               <td>R$ ${(amount*price).toFixed(2)}</td>
                             </tr>`);
+
     listNumber++
-    // nota = $("#nota-body").children()[listNumber];
-    // nota.html()
+    console.log(parseFloat((amount*price).toFixed(2)))
+    totalValueAcc += parseFloat((amount*price).toFixed(2));
+    totalValue.text(`R$ ${totalValueAcc}`)
+
+    // for(var i=0; i < notaBody.lenght(); i++) {
+    //   notaBody.children()[i].children()[5];
+    // }
+
+    $("#valor-total").text()
   
     $("#description").val("");
     $("#amount").val("");
