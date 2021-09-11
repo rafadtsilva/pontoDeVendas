93b49aef7ea82a603b5ad54036f2ac1952e38705
Rafael Trindade <rafaeltrindade@outlook.com>
2021-09-11 10:54:18 -0300

    feat: finalizado checagem das inputs o// :D

--- script.js
+++ script.js
@@ -7,52 +7,38 @@ var realFormat;
 var listNumber = 0;
 var totalValue = $("#valor-total");
 var totalValueAcc = 0;
-const testDescription = /\D{2,}/;
-const testPrice = /[0-9]{3},[0-9]{2}/;
-const testAmount = /[0-2][0-9]/;
+// const testAmount = /[0-9][0-9]+/;
 
-$("#ok").click(lerDados)
+// $("#ok").click(lerDados)
+$("#purchase-data").submit(lerDados);
 
 function lerDados() {
 
   description = $("#description").val();
   amount = parseInt($("#amount").val());
   price = parseFloat($("#price").val());
-  console.log(testDescription.test(description))
 
+  //atualiza última descrição
+  $("#showDescription").text(`${description} (x${amount})`)
+
+  //adiciona a descrição do produto inserido a nota
+  $("#nota-body").append(`<tr>
+                            <td class="pe-3">${listNumber+1}</td>
+                            <td id="description" class="text-start"><span>${description}</span></td>
+                            <td class="px-2">R$ ${price}</td>
+                            <td>${amount}</td>
+                            <td>R$ ${(amount*price).toFixed(2)}</td>
+                          </tr>`);
+
+  listNumber++
+  console.log(parseFloat((amount*price).toFixed(2)))
+  totalValueAcc += parseFloat((amount*price).toFixed(2));
+  totalValue.text(`R$ ${totalValueAcc}`)
+
+  $("#valor-total").text()
   
-  
-  if(testDescription.test(description) !== true || testAmount.test(amount) || description == ""){
-    console.log("Preencha todos os campos corretamente")
-  } else {
+  $("#description").val("");
+  $("#amount").val("");
+  $("#price").val("")
     
-    $("#showDescription").text(`${description} (x${amount})`)
-
-    //adiciona a descrição do produto inserido a nota
-    $("#nota-body").append(`<tr>
-                              <td class="pe-3">${listNumber+1}</td>
-                              <td id="description" class="text-start"><span>${description}</span></td>
-                              <td class="px-2">R$ ${price}</td>
-                              <td>${amount}</td>
-                              <td>R$ ${(amount*price).toFixed(2)}</td>
-                            </tr>`);
-
-    listNumber++
-    console.log(parseFloat((amount*price).toFixed(2)))
-    totalValueAcc += parseFloat((amount*price).toFixed(2));
-    totalValue.text(`R$ ${totalValueAcc}`)
-
-    // for(var i=0; i < notaBody.lenght(); i++) {
-    //   notaBody.children()[i].children()[5];
-    // }
-
-    $("#valor-total").text()
-  
-    $("#description").val("");
-    $("#amount").val("");
-    $("#price").val("")
-    
-  }
-
-
 }
