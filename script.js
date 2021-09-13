94561b82768c2e63bfcbf78f6a7a0be819d2a789
Rafael Trindade <rafaeltrindade@outlook.com>
2021-09-13 20:43:03 -0300

    feat: implementado forma de escrever o produtos e servicos no firestore

--- script.js
+++ script.js
@@ -11,12 +11,36 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
+const PRODUTOS = "Produtos"
+const SERVICES = "Serviços"
 
-db.collection("Produtos").get().then((snapshot) => {
-  snapshot.forEach((doc) => {
-    console.log(doc)
-  })
-})
+// db.collection(SERVICES).doc("inicio").set({
+    
+//   nome: "teste"
+  
+// }).then(doc => {
+//   console.log("doc criado: ", doc)
+// }).catch(err => {
+//   console.log(err)
+// })
+
+// db.collection(PRODUTOS).doc("0001").delete().then(() => {
+//      console.log("Operação realizada com sucesso.")
+//    }).catch (err => {
+//      console.log(err)
+//    })
+
+//DELETADOR
+// db.collection(SERVICES).get().then((snapshot) => {
+//   snapshot.forEach(doc => {
+//     db.collection(SERVICES).doc(doc.id).delete().then(() => {
+//       console.log(`${doc.id} apagado com sucesso`)
+//     }).catch((err) => {
+//       console.log(err)
+//     })
+//   })
+// })
+
 
 var buyer = "Desconhecido";
 var billName;
@@ -25,8 +49,50 @@ var billPrice;
 var twoDecimal;
 var realFormat;
 var listNumber = 0;
+var productNumber = 0;
+var serviceNumber = 0;
 var totalValueOfSpecificRegister = 0;
 var totalValueAcc = 0;
+
+const insertProducs = function(nome, amount, cost, value) {
+  
+  db.collection(PRODUTOS).doc(productNumber.toString()).set({
+
+    nome: nome,
+    amount: amount,
+    cost: cost,
+    value: value
+
+  }).then(doc => {
+    console.log("Produto inserido com sucesso", doc);
+  }).catch(err => {
+    console.log(err);
+  })
+
+  productNumber++
+
+
+}
+
+const insertServices = function(nome, cost, value, amount) {
+  
+  db.collection(SERVICES).doc(serviceNumber.toString()).set({
+
+    nome: nome,
+    amount: amount,
+    cost: cost,
+    value: value
+
+  }).then(doc => {
+    console.log("Produto inserido com sucesso", doc);
+  }).catch(err => {
+    console.log(objeto)
+    console.log(err);
+  })
+
+  serviceNumber++;
+  
+}
 
 var bill = [];
 class billProducts {
@@ -39,16 +105,29 @@ class billProducts {
 }
 
 var products = [];
-class Produto {
-  constructor(name, value, amount) {
-    this.name = name;
-    this.valor = value;
-    this.amount = amount;
-  }
-}
+// class Produto {
+//   constructor(name, value, amount) {
+//     this.name = name;
+//     this.valor = value;
+//     this.amount = amount;
+//   }
+// }
 
-products[0] = new Produto ("Lapis", 0.80, 100);
-console.log(products);
+
+
+insertServices("Xerox P/B", 0.20, 0.50, 999999)
+insertServices("Xerox Color", 0.40, 1.00, 999999)
+insertServices("Impressão P/B", 0.20, 0.50, 999999)
+insertServices("Impressão Color", 0.40, 1.00, 999999)
+insertServices("Curriculo", 1, 4.00, 999999)
+insertServices("Scanner", 0.20, 0.50, 999999)
+
+// products[0] = new Produto ("Lapis", 0.80, 100);
+// console.log(products[0]);
+// var objeto1 = {
+//   nome: "RAfael",
+//   idade: 3
+// }
 
 var purchaseData = document.getElementById("purchase-data");
 
@@ -73,7 +152,7 @@ $("#description").keypress(e => {
 
 $("#purchase-data").submit(registerProducts);
 
-function registerProducts() {
+function registerProducts () {
   
   billName = $("#bill-name").val();
   billAmount = parseInt($("#bill-amount").val());
